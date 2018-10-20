2d985f4a00861c119d4120516d11b9e05267a845
HungerKiller <zhongshui.wang.chn@gmail.com>
2018-10-20 22:48:03 +0800

    add project table

--- src/app/project/project-overview/project-overview.component.ts
+++ src/app/project/project-overview/project-overview.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { JsonReaderService } from '../../services/json-reader.service'
+import { Project } from '../../models/Project';
 
 @Component({
   selector: 'app-project-overview',
@@ -7,11 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectOverviewComponent implements OnInit {
 
+  projects: Project[] = [];
+  cols: any[];
   options: any;
 
-  constructor() { }
+  constructor(private jsonReaderService: JsonReaderService) { }
 
   ngOnInit() {
+    this.jsonReaderService.getJSON("./assets/projects_cn.json").subscribe(data => {
+      this.projects = data['projects'] as Project[];
+    });
+
+    this.cols = [
+      { field: 'name', header: 'Name', width: '30%' },
+      { field: 'organization', header: 'Organization', width: '20%' },
+      { field: 'city', header: 'City', width: '20%' },
+      { field: 'startDate', header: 'StartDate', width: '15%' },
+      { field: 'endDate', header: 'EndDate', width: '15%' }
+    ];
   }
 
 }
--- src/app/project/project.module.ts
+++ src/app/project/project.module.ts
@@ -5,14 +5,16 @@ import { ProjectOverviewComponent } from './project-overview/project-overview.co
 import { ProjectStatisticsComponent } from './project-statistics/project-statistics.component';
 
 import {AccordionModule} from 'primeng/accordion';
-
+import { TableModule } from 'primeng/table';
 import { NgZorroAntdModule, NZ_I18N, zh_CN } from 'ng-zorro-antd';
 import { NgxEchartsModule } from 'ngx-echarts';
+
 
 @NgModule({
   imports: [
     CommonModule,
     AccordionModule,
+    TableModule,
     NgZorroAntdModule,
     NgxEchartsModule
   ],
