12b7b9b06fbeccbf39deb86cfdf48aa69839d48b
HungerKiller <zhongshui.wang.chn@gmail.com>
2018-10-28 23:18:33 +0800

    project gantt

--- src/app/project/project-overview/project-overview.component.ts
+++ src/app/project/project-overview/project-overview.component.ts
@@ -13,20 +13,18 @@ export class ProjectOverviewComponent implements OnInit {
   projects: Project[] = [];
   cols: any[];
   options: any;
-  date: Date;
+  projectNames: string[] = [];
+  startDates: Date[] = [];
+  endDates: Date[] = [];
 
   constructor(private jsonReaderService: JsonReaderService) { }
 
   ngOnInit() {
     this.jsonReaderService.getJSON("./assets/projects_cn.json").subscribe(data => {
       this.projects = data['projects'] as Project[];
-
-      this.date = new Date(this.projects[0].startDate);
-      this.date.setMonth(this.date.getMonth() + 1);
-
-      var day = moment(this.projects[0].startDate).add(1, 'month');
-      console.log(day);
-
+      this.projects.forEach(p => this.projectNames.push(p.name));
+      this.projects.forEach(p => this.startDates.push(this.convertStringToDate(p.startDate)));
+      this.projects.forEach(p => this.endDates.push(this.convertStringToDate(p.endDate, true)));
       this.buildOption();
     });
 
@@ -39,87 +37,77 @@ export class ProjectOverviewComponent implements OnInit {
     ];
   }
 
-  private buildOption()
-  {
+  private convertStringToDate(dateString: string, addingMonth: boolean = false) {
+    let date = new Date(dateString);
+    if (addingMonth)
+      date.setDate(28);
+    return date;
+  }
+
+  private buildOption() {
     this.options = {
       title: {
-        text: '工作流甘特图'
+        text: '项目甘特图'
       },
-      legend: {
-        data: ['计划完成时间', '实际完成时间']
-
+      grid: {
+        top: 50,
+        width: '90%',
+        height: '90%',
+        left: 10,
+        containLabel: true
+      },
+      tooltip: {
+        trigger: 'axis',
+        axisPointer: {            // 坐标轴指示器，坐标轴触发有效
+          type: 'line'        // 默认为直线，可选为：'line' | 'shadow'
+        },
+        formatter: function (params) {
+          var res = params[0].name + "</br>"
+          var date0 = params[0].data;
+          var date1 = params[1].data;
+          date0 = date0.getFullYear() + "-" + (date0.getMonth() + 1);
+          date1 = date1.getFullYear() + "-" + (date1.getMonth() + 1);
+          res += params[0].seriesName + ":" + date0 + "</br>"
+          res += params[1].seriesName + ":" + date1 + "</br>"
+          //console.log(params[0]);
+          return res;
+        }
       },
       xAxis: {
         type: 'time',
         position: "top"
       },
-
       yAxis: {
         type: "category",
-        data: ['测试', '开发', '设计', '总进度']
-
-      },
-      tooltip: {
-        trigger: 'axis',
-        formatter: function(params) {
-          var res = params[0].name + "</br>"
-          var date0 = params[0].data;
-          var date1 = params[1].data;
-          var date2 = params[2].data;
-          date0 = date0.getFullYear() + "-" + (date0.getMonth() + 1) + "-" + date0.getDate();
-          date1 = date1.getFullYear() + "-" + (date1.getMonth() + 1) + "-" + date1.getDate();
-          date2 = date2.getFullYear() + "-" + (date2.getMonth() + 1) + "-" + date2.getDate();
-          res += params[0].seriesName + ":" + date0 + "</br>"
-          res += params[1].seriesName + ":" + date1 + "</br>"
-          res += params[2].seriesName + ":" + date2 + "</br>"
-          //console.log(params[0]);
-          return res;
-        }
+        splitLine: { show: false },
+        data: this.projectNames
       },
       series: [
+
         {
-          name: '计划开始时间',
+          name: '完成时间',
+          type: 'bar',
+          stack: '总量',
+          label: {
+            normal: {
+              show: true,
+              position: 'inside'
+            }
+          },
+          data: this.endDates
+        },
+        {
+          name: '开始时间',
           type: 'bar',
           stack: '总量',
           itemStyle: {
             normal: {
-              color: 'rgba(0,0,0,0)'
+              barBorderColor: 'rgba(255,255,255,1)',
+              color: 'rgba(255,255,255,1)'
             }
           },
-          data: [
-            new Date("2017/09/23"),
-            new Date("2017/09/10"),
-            new Date("2017/09/1"),
-            new Date("2017/09/1"),
-
-          ]
+          data: this.startDates
         },
-
-
-        {
-          name: '计划完成时间',
-          type: 'bar',
-          stack: '总量',
-          data: [
-            new Date("2017/09/31"),
-            new Date("2017/09/20"),
-            new Date("2017/09/15"),
-            new Date("2017/09/30"),
-
-
-          ]
-        }, {
-          name: '实际完成时间',
-          type: 'bar',
-          stack: '总量',
-          data: [
-            new Date("2017/09/30"),
-            new Date("2017/09/23"),
-            new Date("2017/09/14"),
-            new Date("2017/09/30"),
-
-          ]
-        }
       ]
     };
   }
